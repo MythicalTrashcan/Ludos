f92edad733dab6865a83b55faf8c973a9fea3c48
stard <stardust5047@gmail.com>
2022-09-02 22:33:38 -0400

    button

--- script.js
+++
@@ -1,9 +0,0 @@
-const button = document.createElement('button')
-
-button.innerText = 'Button Test'
-button.id = 'mainButton'
-
-button.addEventListener('click', () => {
-    alert('Button worked lol')
-})
-document.body.appendChild(button)
