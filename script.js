3c05b4faa671af90a348ba8f8b2177ddb048b60d
stard <stardust5047@gmail.com>
2022-09-08 22:02:17 -0400

    Wait timer for projects tab and closes old tab

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
+let timeout;
+
 function myFunction() {
 /*    alert("Projects will be added soon!") */
-    window.open("https://mythicaltrashcan.github.io/stard/stard-projects/index.html")
     const button = document.querySelector('button')
     button.disabled = true
+    timeout = setTimeout(alertFunc, 5000);
 }
 
-// let timeout;
-
-// function myFunction() {
-//    alert("Projects will be added soon!")
-//    timeout = setTimeout(alertFunc, 5000);
-// }
-
-// function alertFunc() {
-//    const button = document.querySelector('button')
-// button.disabled = true
-// }
+function alertFunc() {
+window.close();
+window.open("https://mythicaltrashcan.github.io/stard/stard-projects/index.html");
+}
--- stard-projects/README.md
+++ stard-projects/README.md
@@ -1,5 +1,3 @@
 # stard
 ## projects
 All my projects I found/made in a website.
-
- [![HitCount](https://hits.dwyl.com/mythicaltrashcan/https://githubcom/MythicalTrashcan/stard.svg?style=flat-square)](http://hits.dwyl.com/mythicaltrashcan/https://githubcom/MythicalTrashcan/stard)
