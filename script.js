b24ed090a8a5473f068cff4900a2264ae050defa
stard <stardust5047@gmail.com>
2022-09-11 16:01:18 -0400

    View Projects button resets when new tab opens

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
+const { text } = require("express");
+
 let timeout;
 
 function myFunction() {
 /*    alert("Projects will be added soon!") */
     const button = document.querySelector('button')
     button.disabled = true
+    button.textContent = "Loading Tab..."
     timeout = setTimeout(alertFunc, 5000);
 }
 
 function alertFunc() {
-window.close();
-window.open("https://mythicaltrashcan.github.io/stard/stard-projects/index.html");
+    const button = document.querySelector('button')
+    button.textContent = "View Projects"
+    button.disabled = false
+    window.open("https://mythicaltrashcan.github.io/stard/stard-projects/");
 }
