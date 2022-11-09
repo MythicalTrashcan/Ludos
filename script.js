5bac41016939f0ff002bc86b9038286e8654f7ba
stard <stardust5047@gmail.com>
2022-11-09 00:26:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ let timeout;
 
 function myFunction() {
 /*    alert("Projects will be added soon!") */
-    const button = document.querySelector('button')
-    button.disabled = true
-    button.textContent = "Loading Tab..."
+    const button = document.querySelector('button');
+    button.disabled = true;
+    button.textContent = "Loading Tab...";
     timeout = setTimeout(alertFunc, 5000);
 }
 
 function alertFunc() {
-    const button = document.querySelector('button')
-    button.textContent = "View Projects"
-    button.disabled = false
+    const button = document.querySelector('button');
+    button.textContent = "View Projects";
+    button.disabled = false;
     window.open("https://mythicaltrashcan.github.io/stard/stard-projects/");
 }
