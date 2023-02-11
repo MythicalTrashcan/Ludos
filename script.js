4a05eb8969d8f3166b7f45293284e38e0bdecc52
MythicalTrashcan <stardust5047@gmail.com>
2023-02-10 23:21:44 -0500

    update

--- script.js
+++ script.js
@@ -14,10 +14,5 @@ function alertFunc() {
     const button = document.querySelector('button');
     button.textContent = "View Projects";
     button.disabled = false;
-<<<<<<< HEAD
-    window.open("https://mythicaltrashcan.github.io/Ludos/Ludos-projects/");
-}
-=======
     window.open("https://mythicaltrashcan.github.io/Ludos/Ludos-Projects/");
 }
->>>>>>> 13a50350dead7eef93e396d0e6f343ac347ae627
