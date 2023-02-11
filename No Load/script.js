092500ff04875971fb95c52fd4f4d50422dd11bb
MythicalTrashcan <stardust5047@gmail.com>
2023-02-11 03:04:41 -0500

    Pretty big update fix

---
+++ No Load/script.js
@@ -0,0 +1,7 @@
+const { text } = require("express");
+
+let timeout;
+
+function returnMain() {
+    window.open("https://mythicaltrashcan.github.io/Ludos/");
+}
--- script.js
+++ script.js
@@ -1,18 +1,27 @@
-const { text } = require("express");
-
 let timeout;
 
-function myFunction() {
+function viewProjects() {
     /*    alert("Projects will be added soon!") */
     const button = document.querySelector('button');
     button.disabled = true;
     button.textContent = "Loading Tab...";
-    timeout = setTimeout(alertFunc, 5000);
+    timeout = setTimeout(alertFunc, 3000);
 }
 
 function alertFunc() {
     const button = document.querySelector('button');
     button.textContent = "View Projects";
     button.disabled = false;
-    window.open("https://mythicaltrashcan.github.io/Ludos/Ludos-Projects/");
+    window.location.href = 'https://mythicaltrashcan.github.io/Ludos/Ludos-Projects/';
+}
+
+function error404() {
+    window.open("https://mythicaltrashcan.github.io/Ludos/noload.html");
+}
+
+function pageLoadFail() {
+    const button = document.querySelector('button');
+    button.disabled = true;
+    button.textContent = "Loading Tab...";
+    timeout = setTimeout(error404, 4000);
 }
