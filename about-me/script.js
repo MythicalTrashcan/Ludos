8bd02c246e5aece53c895b711241e21618b970ff
MythicalTrashcan <stardust5047@gmail.com>
2023-05-27 00:41:00 -0400

    Fix return button because not working lol

--- about-me/script.js
+++ about-me/script.js
@@ -1,3 +1,7 @@
+function returnMain() {
+    window.location.href = 'https://theludos.com/';
+}
+
 function openGithub() {
     const Github = document.querySelector('github');
     window.open("https://github.com/MythicalTrashcan/");
