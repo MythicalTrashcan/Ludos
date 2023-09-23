6a59cd958bab767fbead5768d9f317ee2f8bff92
MythicalTrashcan <stardust5047@gmail.com>
2023-09-23 07:22:40 -0400

    Add my server to the Eaglercraft games

--- projects/script.js
+++ projects/script.js
@@ -8,3 +8,7 @@ function openAbout() {
     const About = document.querySelector('about');
     window.location.href = 'https://theludos.com/about-me/index.html';
 }
+
+window.addEventListener("load", () => {
+	new ServerEmbed(document.getElementById("embed"), "35%").ping("wss://mc.theludos.com", "" , "" , "" , "hideCracked");
+});
