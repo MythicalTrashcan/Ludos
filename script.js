57319a74ce8a90b4ea81f9160d74f8c4e7fd0195
Mythical Trashcan <stardust5047@gmail.com>
2025-02-17 11:30:05 -0500

    Revamped alt sites code, + Travesty, and - legacy

--- script.js
+++ script.js
@@ -2,44 +2,47 @@ function viewProjects() {
     window.location.href = './projects/';
 }
 
-// Social Links
-
-function openRevolt() {
-    window.open("https://app.revolt.chat/invite/YPnCttyS/");
-}
-
-function openGithub() {
-    window.open("https://github.com/MythicalTrashcan/");
-}
-
-function openYoutube() {
-    window.open("https://www.youtube.com/channel/UCoXIebdrZPGGMrMYVKQpRLg/");
-}
-
-function openAbout() {
-    window.location.href = './about-me/';
-}
-
 function selectMirror() {
     window.location.href = './mirror-links/';
 }
 
+// Social Links
+
+function openTravesty() {
+    alert("If you have not made an account yet, please register and wait for your email. (Emails are not automatically sent; be patient.)");
+    alert("If you do have an account, you can use the code 'NtVBb7Pk' to join the server. (Press the + button on the main page when signed into Travesty.)");
+    
+    var travestyLink = "https://travesty.chat/";
+    var travestyPopup = window.open(travestyLink);
+
+    if (!travestyPopup) {
+        alert("Popup was blocked. Redirecting this page to Travesty's signup page.");
+        window.location.href = travestyLink;
+    }
+}
+
+function openGithub() {
+    var githubLink = "https://github.com/MythicalTrashcan/";
+    var githubPopup = window.open(githubLink);
+
+    if (!githubPopup) {
+        alert("Popup was blocked. Redirecting this page to the Github page.");
+        window.location.href = githubLink;
+    }
+}
+
+function openYoutube() {
+    var youtubeLink = "https://www.youtube.com/channel/UCoXIebdrZPGGMrMYVKQpRLg/";
+    var youtubePopup = window.open(youtubeLink);
+
+    if (!youtubePopup) {
+        alert("Popup was blocked. Redirecting this page to the YouTube page.");
+        window.location.href = youtubeLink;
+    }
+}
+
+// Embed
+
 window.addEventListener("load", () => {
 	new ServerEmbed(document.getElementById("embed"), "35%").ping("wss://mc.theludos.com", "Myth's EagMP" , "true" , "" , "hideCracked");
 });
-
-/* Work on later (eh, not really used in the new setup rn)
-
-/*
-var styles = [{ transform: 'scaleX(.95) scaleY(.95) rotate(3deg)' }, { transform: 'scaleX(.95) scaleY(.95) rotate(-3deg)' }];
-
-function changeStyle() {
-    var randomIndex = Math.floor(Math.random() * styles.length);
-    var selectedStyle = styles[randomIndex];
-
-    var button = document.getElementById('button');
-    for (var property in selectedStyle) {
-        button.style[property] = selectedStyle[property];
-    }
-}
-*/
